c2af0c1665d41a9455b333306d5c94e41d3ee945
Ashesh Shrestha <asheshshrestha2@gmail.com>
2019-10-31 15:07:07 +0545

    nothing

--- SmartWasteMS/trashcan/settings.py
+++ SmartWasteMS/trashcan/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+
 STATICFILES_DIRS = (
     
     os.path.join(BASE_DIR, "static"),
